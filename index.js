2baf12cc0bc5ecd9e44b425d9c6fe94fecddc6ff
DESKTOP-S0OAMUC\northka.chen <northka.chen@gmail.com>
2017-01-16 15:11:41 +0800

    poolConfig 添加到engine暴露出来

--- index.js
+++ index.js
@@ -2,6 +2,8 @@
  * Created by northka.chen on 2016/11/28.
  */
 const request = require('./lib/request')
+const poolConfig = require('./lib/poolConfig')
 module.exports = {
-    request
+    request,
+    poolConfig
 }
